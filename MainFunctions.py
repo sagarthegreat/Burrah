d1f08dbb4c833f77b27a10972ed3b7644223cd02
sagarthegreat <82835167+sagarthegreat@users.noreply.github.com>
2023-10-21 18:12:50 -0400

    Good Commit

--- MainFunctions.py
+++ MainFunctions.py
@@ -2,92 +2,128 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-def pose_similarity(detected_pose, reference_pose):
-    distances = np.linalg.norm(detected_pose - reference_pose, axis=1)
-    return np.mean(distances)
+# Initialize MediaPipe Pose Detection
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+tpose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
+# Define the joint connections
+joint_connections = [
+    ('LEFT_SHOULDER', 'LEFT_ELBOW', 'LEFT_WRIST'),
+    ('RIGHT_SHOULDER', 'RIGHT_ELBOW', 'RIGHT_WRIST'),
+    ('LEFT_SHOULDER', 'LEFT_HIP', 'LEFT_KNEE'),
+    ('RIGHT_SHOULDER', 'RIGHT_HIP', 'RIGHT_KNEE'),
+    ('LEFT_HIP', 'LEFT_KNEE', 'LEFT_ANKLE'),
+    ('RIGHT_HIP', 'RIGHT_KNEE', 'RIGHT_ANKLE'),
+    ('LEFT_ELBOW','LEFT_SHOULDER','LEFT_HIP'),('RIGHT_ELBOW','RIGHT_SHOULDER','RIGHT_HIP')
+]
+
+
+# Function to calculate the angle between three points
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    ba = a - b
+    bc = c - b
+
+    angle_rad = np.arccos(np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc)))
+    angle_deg = np.degrees(angle_rad)
+
+    return angle_deg
+
+
+# Function to annotate angles on an image
+def annotate_angles(frame, joint_connections, landmarks, mp_pose):
+    i = 1
+    angles = []
+    for joint_connection in joint_connections:
+        joint1, joint2, joint3 = joint_connection
+        point1 = [landmarks[getattr(mp_pose.PoseLandmark, joint1).value].x,
+                  landmarks[getattr(mp_pose.PoseLandmark, joint1).value].y]
+        point2 = [landmarks[getattr(mp_pose.PoseLandmark, joint2).value].x,
+                  landmarks[getattr(mp_pose.PoseLandmark, joint2).value].y]
+        point3 = [landmarks[getattr(mp_pose.PoseLandmark, joint3).value].x,
+                  landmarks[getattr(mp_pose.PoseLandmark, joint3).value].y]
+
+        angle = calculate_angle(point1, point2, point3)
+        angle_text = f"{joint1}-{joint2}-{joint3} Angle: {angle:.2f} degrees"
+        angles.append(angle)
+        cv2.putText(frame, angle_text, (10, 30 * i), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        i += 1
+
+    return angles
+
+
+# Function to compare angles and return the median difference
+def compare_angles(previous_angles, current_angles):
+    differences = [abs(curr - prev) for curr, prev in zip(current_angles, previous_angles)]
+    median_difference = np.median(differences)
+    return median_difference
+
+
+# Function to run pose similarity comparison
 def run_pose_similarity(video_file, camera_index, similarity_score_threshold=0.5):
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose()
-
-    # Open the video file for playback
+    # Video capture for the pre-recorded video
     cap_video = cv2.VideoCapture(video_file)
 
-    # Open the webcam for live camera feed
-    cap_camera = cv2.VideoCapture(camera_index)  # 0 represents the default camera
+    # Video capture for the live camera feed
+    cap_camera = cv2.VideoCapture(camera_index)
+    cap_camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap_camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-    # Track the pause state and previous video frame
+    # Initialize variables for pausing and previous video frame
     is_paused = False
     oret_video, oframe_video = None, None
-
+    fc = 0
     while True:
         # Read a frame from the video file if it's not paused
         if not is_paused:
-            ret_video, frame_video = cap_video.read()
-            oret_video, oframe_video = ret_video, frame_video
+            ret_video, vframe = cap_video.read()
+            oret_video, oframe_video = ret_video, vframe
         else:
-            ret_video, frame_video = oret_video, oframe_video
+            ret_video, vframe = oret_video, oframe_video
 
         # Read a frame from the camera
-        ret_camera, frame_camera = cap_camera.read()
+        ret_camera, cframe = cap_camera.read()
 
         if not ret_video or not ret_camera:
             break
 
-        rgb_cframe = cv2.cvtColor(frame_camera, cv2.COLOR_BGR2RGB)
-        cresults = pose.process(rgb_cframe)
+        crgb_frame = cv2.cvtColor(cframe, cv2.COLOR_BGR2RGB)
+        cresults = pose.process(crgb_frame)
 
-        rgb_vframe = cv2.cvtColor(frame_video, cv2.COLOR_BGR2RGB) if frame_video is not None else None
-
+        rgb_vframe = cv2.cvtColor(vframe, cv2.COLOR_BGR2RGB) if vframe is not None else None
         if rgb_vframe is not None:
-            vresults = pose.process(rgb_vframe)
+            vresults = tpose.process(rgb_vframe)
 
-        if cresults.pose_landmarks is not None and vresults.pose_landmarks is not None:
-            cpose = np.array([[lm.x, lm.y] for lm in cresults.pose_landmarks.landmark])
-            vpose = np.array([[lm.x, lm.y] for lm in vresults.pose_landmarks.landmark])
-            similarity_score = pose_similarity(cpose, vpose)
-            print(f'Similarity Score: {similarity_score}')
+        if cresults.pose_landmarks:
+            clandmarks = cresults.pose_landmarks.landmark
+            cangles = annotate_angles(cframe, joint_connections, clandmarks, mp_pose)
+            mp_drawing.draw_landmarks(cframe, cresults.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-            # Draw landmarks and lines on the camera frame
-            try:
-                for lm in cresults.pose_landmarks.landmark:
-                    h, w, _ = frame_camera.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv2.circle(frame_camera, (cx, cy), 5, (0, 255, 0), -1)
+        if vresults.pose_landmarks:
+            vlandmarks = vresults.pose_landmarks.landmark
+            vangles = annotate_angles(vframe, joint_connections, vlandmarks, mp_pose)
+            mp_drawing.draw_landmarks(vframe, vresults.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-                # Draw landmarks and lines on the video frame
-                for lm in vresults.pose_landmarks.landmark:
-                    h, w, _ = frame_video.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv2.circle(frame_video, (cx, cy), 5, (0, 255, 0), -1)
-
-                # Connect landmarks with lines
-                for connection in mp_pose.POSE_CONNECTIONS:
-                    first_landmark = cresults.pose_landmarks.landmark[connection[0]]
-                    second_landmark = cresults.pose_landmarks.landmark[connection[1]]
-                    x1, y1 = int(first_landmark.x * w), int(first_landmark.y * h)
-                    x2, y2 = int(second_landmark.x * w), int(second_landmark.y * h)
-                    cv2.line(frame_camera, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-                    first_landmark = vresults.pose_landmarks.landmark[connection[0]]
-                    second_landmark = vresults.pose_landmarks.landmark[connection[1]]
-                    x1, y1 = int(first_landmark.x * w), int(first_landmark.y * h)
-                    x2, y2 = int(second_landmark.x * w), int(second_landmark.y * h)
-                    cv2.line(frame_video, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            except:
-                pass
-
-            if similarity_score < similarity_score_threshold:
+        # Compare the angles and calculate the similarity score
+        similarity = compare_angles(cangles, vangles)
+        print(similarity)
+        if fc%30 == 0:
+            if similarity < 5:
                 is_paused = True  # Pause the video if the score goes below the threshold
             else:
                 is_paused = False  # Unpause the video
 
         if not is_paused:
             # Show video frame only if it's not paused
-            cv2.imshow('Video Playback', frame_video)
+            cv2.imshow('Video Playback', vframe)
 
-        cv2.imshow('Camera Feed', frame_camera)
-
+        cv2.imshow('Camera Feed', cframe)
+        fc+=1
         if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' key to exit
             break
 
@@ -96,5 +132,6 @@ def run_pose_similarity(video_file, camera_index, similarity_score_threshold=0.5
     cap_camera.release()
     cv2.destroyAllWindows()
 
+
 # Example usage:
-run_pose_similarity(r"C:\Users\sagar\iCloudPhotos\Photos\RPReplay_Final1695854330.mp4", 0)
+run_pose_similarity(r"C:\Users\sagar\OneDrive\Pictures\Camera Roll\WIN_20231021_14_55_21_Pro.mp4", 0)
--- main.py
+++ main.py
@@ -2,15 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+
+app = Flask(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
